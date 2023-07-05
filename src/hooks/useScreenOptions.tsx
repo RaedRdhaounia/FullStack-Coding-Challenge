7a416981ca3406d452f55dce4c42893dd1b5a779
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-05 10:29:27 +0100

    fix header
    fix header as template suggested

--- src/hooks/useScreenOptions.tsx
+++ src/hooks/useScreenOptions.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import {Text} from 'react-native';
-
-import {StackHeaderOptions} from '@react-navigation/stack/lib/typescript/src/types';
+import { Text } from 'react-native';
+import { StackHeaderOptions } from '@react-navigation/stack/lib/typescript/src/types';
 import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import { HomeScreenNavigationProp } from 'constants/types/Tscreens';
+
+type optionT = {
+  stack: NativeStackNavigationOptions;
+  home: NativeStackNavigationOptions;
+  details: NativeStackNavigationOptions;
+  favourites: NativeStackNavigationOptions;
+};
 
 export default () => {
-  const navigation = useNavigation()
+    const navigation = useNavigation<HomeScreenNavigationProp>();
+
   const header = {
-    headerStyle: {elevation: 0, backgroundColor :"transparent"},
-    headerTitleAlign: 'left',
-    headerTitleContainerStyle: {marginLeft: 0},
-    headerLeftContainerStyle: {paddingLeft: 0},
-    headerRightContainerStyle: {paddingRight: 0},
-    headerTitle: ({children}: {children : string}) => (
-      <Text>{children}</Text>
-    ),
+    headerStyle: { backgroundColor: 'transparent' },
+    headerTitleAlign: 'center',
+    headerTitleContainerStyle: { marginLeft: 0 },
+    headerLeftContainerStyle: { paddingLeft: 0 },
+    headerRightContainerStyle: { paddingRight: 0 },
+    headerTitle: ({ children }: { children: string }) => <Text>{children}</Text>,
   } as StackHeaderOptions;
 
   const options = {
@@ -22,22 +29,28 @@ export default () => {
     home: {
       ...header,
       headerTitle: () => null,
-      headerRight: () => <Text onPress={() => navigation.navigate('Favourites')}> favourite</Text>,
-      headerLeft: () => null
+      headerRight: () => (
+        <Text onPress={() => navigation.navigate('Favourites')}>favourite</Text>
+      ),
+      headerLeft: () => null,
     },
-    details : {
-        ...header,
-        headerTitle: () => null,
-        headerRight: () => <Text onPress={() => console.log("dispatch add to favourites")}> favourite + </Text>,
-        headerLeft: () => <Text onPress={() => navigation.goBack()}> back </Text>
-      },
-      favourites : {
-        ...header,
-        headerTitle: () => <Text> favourite + </Text>,
-        headerRight: () => null,
-        headerLeft: () => <Text onPress={() => navigation.goBack()}> back </Text>
-      },
+    details: {
+      ...header,
+      headerTitle: () => null,
+      headerRight: () => (
+        <Text onPress={() => console.log('dispatch add to favourites')}>favourite +</Text>
+      ),
+      headerLeft: () => (
+        <Text onPress={() => navigation.goBack()}>{'<-'}</Text>
+      ),
+    },
+    favourites: {
+      ...header,
+      headerTitle: () => <Text>favourite</Text>,
+      headerRight: () => null,
+      headerLeft: () => null,
+    },
   };
 
-  return options;
+  return options as optionT;
 };
--- src/navigation/Screens.tsx
+++ src/navigation/Screens.tsx
@@ -3,24 +3,33 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { DetailsScreen, FavouritesScreens, HomeScreen} from '../screen';
 import { RootStackParamList } from 'constants/types/Tscreens';
+import { useScreenOptions } from '../hooks';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default () => {
 
+export default () => {
+  const screenOptions = useScreenOptions();
+  const {details, favourites, home, stack} = screenOptions
   return (
-    <Stack.Navigator >
+    <Stack.Navigator screenOptions={stack} >
       <Stack.Screen
         name="Home"
         component={HomeScreen}
+        options={home}
+
       />
     <Stack.Screen
         name="Details"
         component={DetailsScreen}
+        options={details}
+
       />
             <Stack.Screen
         name="Favourites"
         component={FavouritesScreens}
+        options={favourites}
+
       />
     </Stack.Navigator>
   );
