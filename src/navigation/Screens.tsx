17cfe85fea413a119a89bde5c11aa475d6b2fc38
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-05 09:21:42 +0100

    Create Screens.tsx
    add screen stack navigation

---
+++ src/navigation/Screens.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+
+import { DetailsScreen, FavouritesScreens, HomeScreen} from '@screen';
+import { RootStackParamList } from 'constants/types/Tscreens';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default () => {
+
+  return (
+    <Stack.Navigator >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+      />
+    <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+      />
+            <Stack.Screen
+        name="Favourites"
+        component={FavouritesScreens}
+      />
+    </Stack.Navigator>
+  );
+};
