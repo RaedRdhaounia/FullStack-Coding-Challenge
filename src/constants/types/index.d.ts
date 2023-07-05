f6ac497b5739ea830f4746b05f11dc3f3cf4311f
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-05 09:02:07 +0100

    screen types
    add screen types for navigation and stack navigator

---
+++ src/constants/types/index.d.ts
@@ -0,0 +1,8 @@
+declare module '@react-navigation/stack' {
+    export {
+      StackScreenProps,
+      StackNavigationProp,
+      StackHeaderProps,
+      StackNavigationOptions,
+    } from '@react-navigation/stack/lib/typescript/src/types';
+  }
