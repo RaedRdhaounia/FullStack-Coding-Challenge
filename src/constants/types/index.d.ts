d79c8377a074ec3008cb6ad04fd90f974a588d04
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-06 09:32:24 +0100

    Delete index.d.ts

--- src/constants/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@react-navigation/stack' {
-    export {
-      StackScreenProps,
-      StackNavigationProp,
-      StackHeaderProps,
-      StackNavigationOptions,
-    } from '@react-navigation/stack/lib/typescript/src/types';
-  }
