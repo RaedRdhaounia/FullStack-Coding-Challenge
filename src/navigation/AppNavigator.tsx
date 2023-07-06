dab9099029eb6c9329ed25eceb8e5f414de6e736
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-06 01:28:19 +0100

    Update AppNavigator.tsx
    add comments s documentation for AppNavigator function component

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,13 +1,22 @@
+// ==============================|| AppNavigator module ||============================== //
+
+// ==============================|| IMPORTS
 import React from 'react';
+//-- container navigation imports
 import { NavigationContainer } from '@react-navigation/native';
+
+//-- Stack screen imports
 import Screens from "./Screens"
 
-const AppNavigator: React.FC = () => {
-  return (
-    <NavigationContainer>
-        <Screens/>
-    </NavigationContainer>
-  );
-};
+// ==============================|| AppNavigator component||============================== //
+/**
+ * manages our navigation tree
+ * @name: AppNavigator
+ * @returns React.FC.
+ * @example
+ * <AppNavigator/> 
+ */
+
+const AppNavigator: React.FC = () =>  ( <NavigationContainer children={<Screens/>}/> );
 
 export default AppNavigator;
