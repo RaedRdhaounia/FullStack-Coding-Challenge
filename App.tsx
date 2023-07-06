e5260a3fa9ec126ed10f6a752347a922ae1bffd1
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-06 17:21:44 +0100

    Update App.tsx
    connect redux to the project using react-redux provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {AppNavigator} from './src/navigation';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
