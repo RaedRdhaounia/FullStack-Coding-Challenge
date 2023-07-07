4d1dd041950a25e4b4d03864ca75374e07aa12f8
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-07 04:14:15 +0100

    Update Card.tsx
    clean up card

--- src/components/others/homeScreenCard/Card.tsx
+++ src/components/others/homeScreenCard/Card.tsx
@@ -1,59 +1,130 @@
-// ==============================|| Card module ||============================== //
+// ==============================|| MovieList module ||============================== //
 
 // ==============================|| IMPORTS
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-//-- native component imports
-import {View, Text, Image, StyleSheet} from 'react-native';
+//-- native components imports
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 
-//-- single movieCard info type imports
+//-- prop types imports
 import {Movie} from '../../../constants/types/reduxState';
 
-// ==============================|| Card component||============================== //
+// ==============================|| MovieList component ||============================== //
+
+//-------- local component props interface
+interface MovieListP {
+  movies: Movie[];
+  searchTerm: string;
+  navigation: any;
+}
+
 /**
- * manages our navigation tree
- * @name: Card
- * @returns {JSX.Element}.
+ *
+ * @name MovieList
+ * @returns React.FC
  * @example
- * const data = {item : {poster: "http://...", title: "title", averageRating: 20}}
- * <Card {...data} />
+ * <MovieList navigation={navigation} movies={movies} searchTerm={searchTerm} />
  */
-const Card = ({item}: {item: Movie}): JSX.Element => {
+const MovieList: React.FC<MovieListP> = ({movies, searchTerm, navigation}) => {
+  //============ hooks
+  //-- local State
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  //-- update component depends on searchTerm
+  useEffect(() => {
+    const filtered = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredMovies(filtered);
+  }, [movies, searchTerm]);
+
+  // ==============================|| MovieList component ||============================== //
+
+  const renderCard = ({item}: {item: Movie}) => {
+    const handleNavigate = (id: number) => {
+      navigation.navigate('Details', {itemId: id});
+    };
+
+    return (
+      <TouchableOpacity
+        style={styles.cardContainer}
+        onPress={() => handleNavigate(item.id)}>
+        <View style={styles.card}>
+          <Image source={{uri: item.poster}} style={styles.image} />
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.rating}>{item.averageRating}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  // ==============================|| render component
   return (
-    <View style={styles.card}>
-      <Image source={{uri: item.poster}} style={styles.image} />
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.rating}>{item.averageRating}</Text>
-    </View>
+    <FlatList
+      data={filteredMovies}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={2}
+      renderItem={renderCard}
+      contentContainerStyle={styles.container}
+    />
   );
 };
 
-export default Card;
-
+// ==============================|| styles
 const styles = StyleSheet.create({
-  card: {
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  cardContainer: {
     flex: 1,
     margin: 8,
     borderRadius: 8,
     backgroundColor: '#fff',
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  card: {
+    flex: 1,
+    borderRadius: 8,
+    overflow: 'hidden',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 16,
   },
   image: {
     width: 150,
-    height: 200,
+    height: 150,
     borderRadius: 8,
     marginBottom: 8,
   },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   title: {
+    flex: 1,
     fontSize: 16,
     fontWeight: 'bold',
-    marginBottom: 4,
+    marginRight: 8,
   },
   rating: {
     fontSize: 14,
     color: 'gray',
   },
 });
+
+export default MovieList;
