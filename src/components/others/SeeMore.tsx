0ef5771e0266382afdf571848060d2f4c8cac7ef
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-07-07 06:17:58 +0100

    add documentation

--- src/components/others/SeeMore.tsx
+++ src/components/others/SeeMore.tsx
@@ -1,16 +1,37 @@
-import {fetchTopRatedMovies} from '../../utils';
+// ==============================|| SeeMore module ||============================== //
+
+// ==============================|| IMPORTS
+
 import React, {useState} from 'react';
+
+//-- native components imports
 import {Button} from 'react-native';
+
+//-- redux imports
 import {useDispatch, useSelector} from 'react-redux';
 import {setCurrentPage} from '../../redux/favoriteMoviesListSlice';
 
+//-- thankAction imports
+import {fetchTopRatedMovies} from '../../utils';
+
+// ==============================|| SeeMore component ||============================== //
+
 const SeeMore = () => {
+  //============ redux
+  //-- destruction dispatch method
   const dispatch = useDispatch();
+  //-- selector store variables method
   const store = useSelector((state: any) => state.movies);
+  //-- getting loading and pages needs at this component
   const page = store.currentPage;
   const loading = store.loading;
-  console.log('loading', loading);
+
+  //============ hooks
+  //-- local State
   const [CurrentPage, setPage] = useState<number>(page);
+
+  //============ methods
+  //-- load movies function
   const loadMoreMovies = () => {
     dispatch(fetchTopRatedMovies(CurrentPage + 1));
     dispatch(setCurrentPage(CurrentPage + 1));
@@ -18,6 +39,9 @@ const SeeMore = () => {
   };
   if (loading === 'pending') {
     return <Button title="loading please wait..." disabled />;
+  }
+  if (loading === 'succeeded') {
+    return <Button title="load more" onPress={loadMoreMovies} color="green" />;
   }
   return <Button title="load more" onPress={loadMoreMovies} />;
 };
