8c556803e4a78018797e18a4da84edbd73f36347
raed rdhaounia <raedrdhaounia@gmail.com>
2023-07-08 21:15:52 +0100

    Update TextInputSearch.tsx
    update input background color

--- src/components/generator/TextInputSearch.tsx
+++ src/components/generator/TextInputSearch.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginHorizontal: 10,
     marginBottom: 10,
+    backgroundColor: "#999ED7",
   },
   iconContainer: {
     marginRight: 10,
